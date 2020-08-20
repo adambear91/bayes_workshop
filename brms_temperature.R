5a7081b4ff04681a6073f9f1fe73285947f8eada
adambear91 <adambear@gmail.com>
2020-08-20 11:23:21 -0400

    brms

--- brms_temperature.R
+++ brms_temperature.R
@@ -77,7 +77,7 @@ data %>%
   geom_abline(intercept = mean(samples$b_Intercept), # plots the average line from the posterior distribution
               slope     = mean(samples$b_x_std), color = "red", size = 1.5) +
   geom_abline(intercept = samples[1:50, 1], # plots many lines from the posterior distribution
-              slope     = samples[1:50, 2], color = "red", alpha = 0.3) +
+              slope     = samples[1:50, 2], color = "black", alpha = 0.3) +
   geom_point() +
   labs(x = "Need for cognition (std)", y = "Shower temperature preference") +
   #geom_smooth(method = "lm", alpha = 0) +
@@ -86,7 +86,7 @@ data %>%
 # Using shaded interval to indicate uncertainty
 # To do this we need to discretize the predictor and compute the posterior distribution of the mean for each discrete predictor value
 # Here is how you would do this for a single discrete value of the predictor (x = 1)
-mu_at_1 <- samples %>% 
+mu_at_1 <- samples %>%
   mutate(mu_at_1 = b_Intercept + b_x_std * 1) # remember the mean in our model is the sum of the intercept and slope
 
 # Plot
@@ -105,7 +105,7 @@ mu_long <- fitted(fit_bayes, newdata = newdata, summary = F) %>% # the summary a
   as_tibble() %>%
   set_names(x_values) # sets the column names as our discrete values of the predictor variable
 
-head(mu_long) # inspect 
+head(mu_long) # inspect
 mean(mu_long$`1`) # this should match the mean value of mu_at_1
 mean(mu_at_1$mu_at_1)
 
@@ -131,7 +131,7 @@ data %>% # now we can plot the shaded region using the geom smooth function and 
 # How do we do this?
 # We need to additionally take into account the posterior distribution over sigma
 # Here is how you would do this for a single discrete value of the predictor (x = 1)
-y_at_1 <- samples %>% 
+y_at_1 <- samples %>%
   mutate(y_at_1 = rnorm(n = nrow(.), mean = b_Intercept + b_x_std * 1, sd = sigma)) # now we are sampling data from the model, averaged over the posterior
 
 # Plot
@@ -152,7 +152,7 @@ y_long <- predict(fit_bayes, newdata = newdata, summary = F) %>% # the summary a
   as_tibble() %>%
   set_names(x_values)
 
-head(y_long) # inspect 
+head(y_long) # inspect
 mean(y_long$`1`) # this should approximately match the mean value of y_at_1 (approximately because of sampling variation)
 mean(y_at_1$y_at_1)
 
@@ -170,7 +170,7 @@ data %>% # now we can plot uncertainty in the model's posterior prediction inter
               aes(y = Estimate, ymin = Q2.5, ymax = Q97.5),
               stat = "identity",
               fill = "red", color = "red", alpha = 0.2, size = 1.5) +
-  geom_ribbon(data = y_summary, 
+  geom_ribbon(data = y_summary,
               aes(ymin = Q2.5, ymax = Q97.5),
               fill = "red", alpha = 0.2) +
   labs(x = "Need for cognition (std)", y = "Shower temperature preference") +
@@ -180,7 +180,7 @@ data %>% # now we can plot uncertainty in the model's posterior prediction inter
 # Mean-centering in brms ####
 # Remember how we standardized the predictor variable in our previous model? This is helpful for several reasons
 # 1. It makes it easier to set priors on the intercept
-# 2. It makes MCMC sampling more efficent
+# 2. It makes MCMC sampling more efficient
 # 3. brms internally centers your predictor variables before sampling (because of 2.)
 # Read more here https://bookdown.org/content/3890/horoscopes-insights.html#use-the-0-intercept-syntax
 
@@ -213,5 +213,5 @@ samples_2 %>%
   geom_vline(xintercept = mean(samples_2$b_x), color = "red") +
   geom_hline(yintercept = mean(samples_2$b_Intercept), color = "red")
 
-# Hint: 
+# Hint:
 plot(data$x, data$y)
